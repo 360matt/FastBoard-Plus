0ebf647b1a8a4229f18171d11f3ec09baa647bdc
360matt <mattbacq2004@gmail.com>
2021-08-04 23:53:27 +0200

    Registering enable/disable API

--- src/main/java/fr/i360matt/fastboardplus/ScoreBoardAPI.java
+++ src/main/java/fr/i360matt/fastboardplus/ScoreBoardAPI.java
@@ -1,62 +1,69 @@
 package fr.i360matt.fastboardplus;
 
-
-
 import org.bukkit.Bukkit;
-import org.bukkit.plugin.Plugin;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 
+public class ScoreBoardAPI implements Listener {
 
-public class ScoreBoardAPI {
-    protected static BoardPlus defaultInstance;
+    protected static ScheduledExecutorService timer;
+    protected static boolean enabled = false;
 
-    protected static Plugin registeredPlugin, disabled;
-    public static void registerPlugin (final Plugin plugin) {
-        Bukkit.getPluginManager().registerEvents(new BoardEvents(), plugin);
-        registeredPlugin = plugin;
-        startService();
-    }
-
-    public static void unregisterPlugin () {
-        if (registeredPlugin != null) {
-            BoardPlus.timer.shutdownNow();
-            BoardPlus.deleteAll();
-            registeredPlugin = null;
-            stopService();
-        }
+    public static boolean isEnabled () {
+        return enabled;
     }
 
     public static void disable () {
-        if (registeredPlugin != null) {
-            disabled = registeredPlugin;
-            registeredPlugin = null;
-            stopService();
+        enabled = false;
+        for (final BoardView boardPlus : BoardView.boards.values()) {
+            boardPlus.removeAllPlayers();
         }
     }
 
     public static void enable () {
-        if (disabled != null) {
-            registeredPlugin = disabled;
-            disabled = null;
-            startService();
-        }
+        enabled = true;
+        BoardPlayer.setAllToDefault();
     }
 
-    private static void startService () {
-        if (BoardPlus.timer == null || BoardPlus.timer.isShutdown())
-            BoardPlus.timer = Executors.newScheduledThreadPool(128);
-    }
-
-    private static void stopService () {
-        if (BoardPlus.timer != null && !BoardPlus.timer.isShutdown())
-            BoardPlus.timer.shutdownNow();
+    public static void registerPlugin (final JavaPlugin plugin) {
+        timer = Executors.newSingleThreadScheduledExecutor();
+        Bukkit.getPluginManager().registerEvents(new ScoreBoardAPI(), plugin);
+        enabled = true;
     }
 
 
-    /**
-     *
-     * @return l'instance qui a été register par défaut
-     */
-    public static BoardPlus getDefaultInstance () { return defaultInstance; }
+    /* start interscoarboard events private methods */
+    @EventHandler
+    private static void eventMove (final PlayerMoveEvent event) {
+        if (!event.getFrom().getBlock().getLocation().equals(event.getTo().getBlock().getLocation()))
+            for (final BoardView instanceCandidate : BoardView.boards.values())
+                for (final BoardPlayer pBoardCandidate : instanceCandidate.getAllPlayerBoards())
+                    instanceCandidate.onMove(pBoardCandidate);
+    }
+
+    @EventHandler
+    private static void eventJoin (final PlayerJoinEvent event) {
+        if (BoardView.defaultInstance != null)
+            BoardPlayer.getPlayer(event.getPlayer()).setCurrent(BoardView.defaultInstance);
+
+        for (final BoardView instanceCandidate : BoardView.boards.values())
+            for (final BoardPlayer pBoardCandidate : instanceCandidate.getAllPlayerBoards())
+                instanceCandidate.onOnlineChange(pBoardCandidate);
+    }
+
+    @EventHandler
+    private static void eventQuit (final PlayerQuitEvent event) {
+        BoardPlayer.deletePlayer(event.getPlayer());
+
+        for (final BoardView instanceCandidate : BoardView.boards.values())
+            for (final BoardPlayer pBoardCandidate : instanceCandidate.getAllPlayerBoards())
+                instanceCandidate.onOnlineChange(pBoardCandidate);
+    }
 }
